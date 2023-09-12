2bc46db795363732f4cd012522638dab4419862b
mouryaavadhesh <mourya.avadhesh@gmail.com>
2023-09-12 21:10:15 +0530

    read write into file

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamond", newCards()}
-	cards = append(cards, "Six of Spades")
+	//cards := newDeck()
+	// hand, remainingCards := deal(cards, 5)
+	// cards.print()
+	// hand.print()
+	// remainingCards.print()
+	//fmt.Println(cards.toString())
+	//cards.saveToFile("my_cards")
+	cards := newDeckFromFile("my_cards")
 	cards.print()
-
-}
-
-func newCards() string {
-	return "Five of Diamonds"
 }
