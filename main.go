4c9f7fd7513b8a6f1ec087b32f04f28c89ce8781
mouryaavadhesh <mourya.avadhesh@gmail.com>
2023-09-13 16:39:37 +0530

    suffle

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 func main() {
-	//cards := newDeck()
+	cards := newDeck()
 	// hand, remainingCards := deal(cards, 5)
 	// cards.print()
 	// hand.print()
 	// remainingCards.print()
 	//fmt.Println(cards.toString())
 	//cards.saveToFile("my_cards")
-	cards := newDeckFromFile("my_cards")
+	//cards := newDeckFromFile("my_cards")
+	cards.shuffle()
 	cards.print()
 }
