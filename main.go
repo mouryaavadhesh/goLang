ecd9437ffc7840e7181f6c5ff6e2ca91bc47acea
mouryaavadhesh <mourya.avadhesh@gmail.com>
2023-09-14 22:28:32 +0530

    pointer and struct

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
+import "fmt"
+
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
 func main() {
-	cards := newDeck()
+	//cards := newDeck()
 	// hand, remainingCards := deal(cards, 5)
 	// cards.print()
 	// hand.print()
@@ -9,6 +22,25 @@ func main() {
 	//fmt.Println(cards.toString())
 	//cards.saveToFile("my_cards")
 	//cards := newDeckFromFile("my_cards")
-	cards.shuffle()
-	cards.print()
+	// cards.shuffle()
+	// cards.print()
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+	jimPointer := &jim
+	jimPointer.updateName("jimmy")
+	jim.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
