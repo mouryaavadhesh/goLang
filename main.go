14e9be8c77613c3864aac2b916d37913b1f37317
mouryaavadhesh <mourya.avadhesh@gmail.com>
2023-09-15 16:51:29 +0530

    interface

--- main.go
+++ main.go
@@ -13,6 +13,15 @@ type person struct {
 	lastName  string
 	contactInfo
 }
+
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+
 func main() {
 	//cards := newDeck()
 	// hand, remainingCards := deal(cards, 5)
@@ -24,17 +33,22 @@ func main() {
 	//cards := newDeckFromFile("my_cards")
 	// cards.shuffle()
 	// cards.print()
-	jim := person{
-		firstName: "Jim",
-		lastName:  "Party",
-		contactInfo: contactInfo{
-			email:   "jim@gmail.com",
-			zipCode: 94000,
-		},
-	}
-	jimPointer := &jim
-	jimPointer.updateName("jimmy")
-	jim.print()
+	// jim := person{
+	// 	firstName: "Jim",
+	// 	lastName:  "Party",
+	// 	contactInfo: contactInfo{
+	// 		email:   "jim@gmail.com",
+	// 		zipCode: 94000,
+	// 	},
+	// }
+	// jimPointer := &jim
+	// jimPointer.updateName("jimmy")
+	// jim.print()
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
 }
 
 func (pointerToPerson *person) updateName(newFirstName string) {
@@ -43,4 +57,16 @@ func (pointerToPerson *person) updateName(newFirstName string) {
 
 func (p person) print() {
 	fmt.Printf("%+v", p)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	return "Hi there!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola!"
 }
